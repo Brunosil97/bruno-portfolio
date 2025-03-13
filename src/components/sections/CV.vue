664a57120b1b56278602072f564ee014c69c8d6f
Bruno Silva <brunosilvinosilva@gmail.com>
2025-03-13 15:39:34 +0000

    embedded pdf cv for cv page

--- src/components/sections/CV.vue
+++ src/components/sections/CV.vue
@@ -1,3 +1,15 @@
 <template>
-  cv
+  <div class="mockup-browser border border-base-300 w-full">
+  <div class="mockup-browser-toolbar">
+    <div class="input">https://brunosilva.com</div>
+  </div>
+  <div class="grid h-200">
+    <embed
+      src="../../src/assets/cv_bruno_silva.pdf#navpanes=0"
+      type="application/pdf"
+      width="100%"
+      height="100%"
+    />
+  </div>
+</div>
 </template>
