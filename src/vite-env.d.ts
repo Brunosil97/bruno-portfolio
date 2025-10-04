87604c1d22d21b2c05e81879a03a458316388625
Bruno Silva <bruno.silva@hsf.com>
2025-10-04 14:09:38 +0100

    updated environment checks

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" />
+
 interface ImportMetaEnv {
   readonly VITE_GITHUB_TOKEN: string;
   readonly BASE_URL: string;
@@ -5,4 +7,11 @@ interface ImportMetaEnv {
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
+}
+
+export {}
+
+declare global {
+  // This represents the UI error types that could occur
+  type UIError = string | Error | Record<string, unknown> | undefined;
 }
